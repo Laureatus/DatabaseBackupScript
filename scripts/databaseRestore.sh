92b5646c0c60a5552daed63939dbb735469c41f9
Oliver Holst <oliver.holst@sonova.com>
2023-07-06 11:28:32 +0200

    Adjust variables and add log

--- scripts/databaseRestore.sh
+++ scripts/databaseRestore.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 source ../backupconfig.conf
-mysql --host=$mySqlHost --port=$mySqlPort --user=$userName --password=$password $trgDb < $dbDumpPath
+
+echo "Upload dump file of $SRC_DB to $HOST:$PORT/$TRG_DB" 2>&1 >> $LOG_PATH
+mysql --host=$HOST --port=$PORT --user=$USER_NAME --password=$PASSWORD $TRG_DB < $BACKUP_DIR
